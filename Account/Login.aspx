e22693791df14d45adfc206286659e036a868e81
axelsomerseth <axelsomerseth@hotmail.com>
2016-04-29 19:18:52 -0600

    Added files via upload

---
+++ Account/Login.aspx
@@ -0,0 +1,76 @@
+﻿<%@ Page Title="Iniciar sesión" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true"  CodeBehind="Login.aspx.cs" Inherits="ActiveDirectory.Account.Login" Async="true" %>
+
+<%@ Register Src="~/Account/OpenAuthProviders.ascx" TagPrefix="uc" TagName="OpenAuthProviders" %>
+
+<asp:Content runat="server" ID="BodyContent" ContentPlaceHolderID="MainContent">
+    <h2><%: Title %></h2>
+
+    <div class="row">
+        <div class="col-md-8">
+            <section id="loginForm">
+                <div class="form-horizontal">
+                    <h4>Utilice una cuenta de Active Directory para iniciar sesión.</h4>
+                    <hr />
+                    <asp:PlaceHolder runat="server" ID="ErrorMessage" Visible="false">
+                        <p class="text-danger">
+                            <asp:Literal runat="server" ID="FailureText" />
+                        </p>
+                    </asp:PlaceHolder>
+                    <div class="form-group">
+                        <asp:Label runat="server" AssociatedControlID="Domain" CssClass="col-md-2 control-label">Dominio: </asp:Label>
+                        <div class="col-md-10">
+                            <asp:TextBox runat="server" ID="Domain" CssClass="form-control" TextMode="SingleLine" placeholder="cordova.com"/>
+                            <asp:RequiredFieldValidator runat="server" ControlToValidate="Domain" CssClass="text-danger" ErrorMessage="El dominio es obligatorio." />
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <asp:Label runat="server" AssociatedControlID="Username" CssClass="col-md-2 control-label">Usuario: </asp:Label>
+                        <div class="col-md-10">
+                            <asp:TextBox runat="server" ID="Username" CssClass="form-control" TextMode="SingleLine"  placeholder="escriba aquí su nombre de usuario"/>
+                            <asp:RequiredFieldValidator runat="server" ControlToValidate="Username" CssClass="text-danger" ErrorMessage="El nombre de usuario es obligatorio." />
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <asp:Label runat="server" AssociatedControlID="Password" CssClass="col-md-2 control-label">Contraseña: </asp:Label>
+                        <div class="col-md-10">
+                            <asp:TextBox runat="server" ID="Password" TextMode="Password" CssClass="form-control" />
+                            <asp:RequiredFieldValidator runat="server" ControlToValidate="Password" CssClass="text-danger" ErrorMessage="La contraseña es obligatoria." />
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="col-md-offset-2 col-md-10">
+                            <div class="checkbox">
+                                <asp:CheckBox runat="server" ID="RememberMe" Visible="false"/>
+                                <asp:Label runat="server" AssociatedControlID="RememberMe" Visible="false">¿Recordar cuenta?</asp:Label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="col-md-offset-2 col-md-10">
+                            <asp:Button runat="server" OnClick="ingresar" Text="Acceder" CssClass="btn btn-default" Height="40px" Width="142px" Font-Size="Larger" style="font-weight: bold"/>
+                        </div>
+                    </div>
+                </div>
+                <%--
+                <p>
+                    NO QUIERO REGISTRAR USUARIOS NUEVOS
+                        <asp:HyperLink runat="server" ID="RegisterHyperLink" ViewStateMode="Disabled">Registrarse como usuario nuevo</asp:HyperLink>
+                    
+                </p>
+                --%>
+                <%--<p>
+                     Habilite esta opción cuando haya habilitado la confirmación de cuentas para la funcionalidad de restablecimiento de contraseña 
+                    <asp:HyperLink runat="server" ID="ForgotPasswordHyperLink" ViewStateMode="Disabled" class="col-md-offset-2 col-md-10">Recuperar contraseña</asp:HyperLink>
+                    
+                </p>--%>
+            </section>
+        </div>
+
+        <div class="col-md-4">
+            <section id="socialLoginForm" style="font-weight: bold; font-size: x-large">
+                <br /><br /><br /><br />
+                <asp:label id="lblError" runat="server" class="text-danger"/>
+            </section>
+        </div>
+    </div>
+</asp:Content>
---
+++ Account/Login.aspx.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Web;
+using System.Web.UI;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Owin;
+using ActiveDirectory.Models;
+using System.Web.Security;
+using System.Web.UI.WebControls;
+using System.DirectoryServices;
+
+
+namespace ActiveDirectory.Account
+{
+    public partial class Login : Page
+    {
+        protected DirectoryEntry acceso;
+
+        public string Dominio
+        {
+            get
+            {
+                return Domain.Text.Trim();
+            }
+        }
+        public string Usuario
+        {
+            get
+            {
+                return Username.Text.Trim();
+            }
+        }
+        public string Contrasenia
+        {
+            get
+            {
+                return Password.Text.Trim();
+            }
+        }
+
+
+
+
+
+        protected void ingresar(object sender, EventArgs e)
+        {
+
+            Session["dominio"] = Domain.Text.Trim();
+            Session["usuario"] = Username.Text.Trim();
+            Session["contrasenia"] = Password.Text.Trim();
+
+
+            this.acceso = new DirectoryEntry("LDAP://" + Domain.Text.Trim(),
+                                                         Username.Text.Trim(),
+                                                         Password.Text.Trim(),
+                                                         AuthenticationTypes.Secure); //AuthenticationTypes.Secure como 4to parámetro
+
+            try
+            {
+                DirectorySearcher existe = new DirectorySearcher(acceso);
+                SearchResult resultado = existe.FindOne(); //Devuelve el primero que encuentra
+
+
+                if (resultado != null)
+               {
+                    lblError.Text = "Usted ha sido autenticado";
+                    Server.Transfer("Administrar.aspx", true);
+                    //Response.Redirect("Administrar.aspx");
+                }
+
+
+            }
+            catch
+            {
+                lblError.Text = "Error:     Disculpe, usted no ha sido autenticado. \n Por favor, verifique sus datos.";
+            }
+
+            
+        }
+
+        //fin de ingresar
+
+
+
+
+        /*protected void Page_Load(object sender, EventArgs e)
+        {
+            RegisterHyperLink.NavigateUrl = "Register";
+            // Habilite esta opción una vez tenga la confirmación de la cuenta habilitada para la funcionalidad de restablecimiento de contraseña
+            //ForgotPasswordHyperLink.NavigateUrl = "Forgot";
+            OpenAuthLogin.ReturnUrl = Request.QueryString["ReturnUrl"];
+            var returnUrl = HttpUtility.UrlEncode(Request.QueryString["ReturnUrl"]);
+            if (!String.IsNullOrEmpty(returnUrl))
+            {
+                RegisterHyperLink.NavigateUrl += "?ReturnUrl=" + returnUrl;
+            }
+        }*/
+
+
+        /*
+        protected void LogIn(object sender, EventArgs e)
+        {
+            if (IsValid)
+            {
+                // Validar la contraseña del usuario
+                var manager = Context.GetOwinContext().GetUserManager<ApplicationUserManager>();
+                var signinManager = Context.GetOwinContext().GetUserManager<ApplicationSignInManager>();
+
+                // Esto no cuenta los errores de inicio de sesión hacia el bloqueo de cuenta
+                // Para habilitar los errores de contraseña para desencadenar el bloqueo, cambie a shouldLockout: true
+                var result = signinManager.PasswordSignIn(Domain.Text + Username.Text, Password.Text, RememberMe.Checked, shouldLockout: false);
+
+                switch (result)
+                {
+                    case SignInStatus.Success:
+                        IdentityHelper.RedirectToReturnUrl(Request.QueryString["ReturnUrl"], Response);
+                        break;
+                    case SignInStatus.LockedOut:
+                        Response.Redirect("/Account/Lockout");
+                        break;
+                    case SignInStatus.RequiresVerification:
+                        Response.Redirect(String.Format("/Account/TwoFactorAuthenticationSignIn?ReturnUrl={0}&RememberMe={1}", 
+                                                        Request.QueryString["ReturnUrl"],
+                                                        RememberMe.Checked),
+                                          true);
+                        break;
+                    case SignInStatus.Failure:
+                    default:
+                        FailureText.Text = "Intento de inicio de sesión no válido";
+                        ErrorMessage.Visible = true;
+                        break;
+                }
+            }
+        }
+        */
+
+
+
+    }
+}
